0dfed5a1ba408b12d4af3abcb5c5b2d474c87c52
emili <emili@192.168.1.3>
2024-04-18 23:13:43 -0300

    test

--- ArraysExercises/src/arrays/ExerciseThree.java
+++ ArraysExercises/src/arrays/ExerciseThree.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ExerciseThree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Scanner scan = new Scanner(System.in);
 		
