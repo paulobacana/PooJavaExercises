b6bd3af8e222d07ba9d034d075d18c131255aa96
paulobacana <emilosantos936@gmail.com>
2024-04-18 23:11:23 -0300

    feat: new exercise(still not done)

---
+++ ArraysExercises/src/arrays/ExerciseThree.java
@@ -0,0 +1,28 @@
+package arrays;
+import java.util.Scanner;
+
+public class ExerciseThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int [] arr = {1, 5, 6, 8, 2, 9};
+		int num;
+		
+		System.out.println("digite um número para verificar se está no array:");	
+		num = scan.nextInt();
+		
+		for(int i = 0; i<arr.length; i++) {
+			
+			if (num == arr[i]) {
+				System.out.println(" existe no array");
+			} 
+				System.out.println( "não existe no array");
+		};
+	
+		
+	}
+
+}
